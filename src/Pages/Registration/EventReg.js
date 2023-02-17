c1474576afd12417d66dc07540a8af7a92250d99
Arvindgurjar <88261937+Arvindgurjar@users.noreply.github.com>
2023-02-17 22:03:40 +0530

    o

--- src/Pages/Registration/EventReg.js
+++ src/Pages/Registration/EventReg.js
@@ -7,8 +7,7 @@ import QR from "../../images/qr.jpg"
 import NotFound from '../NotFound';
 import { ToastContainer, toast } from 'react-toastify';
 import Loading from "../../Components/Loading"
-import resetProvider from "ra-data-simple-rest"
-const url = resetProvider("https://codingsoulbackend.onrender.com");
+
 const EventRegi = () => {
   const Navigate = useNavigate();
   const { register, handleSubmit, formState: { errors } } = useForm();
@@ -64,7 +63,7 @@ const EventRegi = () => {
     /* for post data */
     const { fname, lname, enrollment_number, email, contact, gender, college, branch, year, semester, section, language, hacker, tdate, time, tid, check } = user
     try {
-      const res = await fetch(`${url}/Event`, {
+      const res = await fetch("/Event", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
@@ -94,7 +93,7 @@ const EventRegi = () => {
   const show = async () => {
     /* for checking of form is open or closed by admin */
     try {
-      const res = await fetch(`${url}/toggle`, {
+      const res = await fetch("/toggle", {
         method: "GET",
         headers: {
           Accept: "application/json",
